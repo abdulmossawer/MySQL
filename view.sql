9c4a2beede8087d18e388a895763eb849d116de8
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-07-07 12:37:04 +0530

    Update

--- view.sql
+++ view.sql
@@ -2,4 +2,4 @@ SELECT * FROM classic_models_dataset.customers;
 
 create view countOfCustomers as
 select country, count(customerNumber) from classic_models_dataset.customers
-group by country
+group by country;
